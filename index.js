a6840ac806c5b4fac754e81fb1b80adbf193b78d
sebin0611 <sebinpmoncy@gmail.com>
2025-04-24 17:24:53 +0530

    back

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ import  dbconnect  from "./utils/db.js"
 import router from "./routes/index.js";
 
 const app= express()
-const port =3000
+const port = process.env.PORT || 3000;
+
 
 app.use(cors(
     {
